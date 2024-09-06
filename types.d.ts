12af305df6106ffba631e6052a5aad3640dade6a
krishnaheda14 <150182849+krishnaheda14@users.noreply.github.com>
2024-09-06 23:43:31 +0530

    Add files via upload

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+// Define your navigation parameter list for the bottom tab navigator
+export type TabParamList = {
+  Home: undefined;
+  CheckInOut: undefined;
+  WorkHours: undefined;
+  FaceRecognition: undefined;
+  Settings: undefined;
+  Help: undefined;
+};
